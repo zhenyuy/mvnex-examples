f12d500e04df4401dceff9d4abb16a709fd84b58
zhenyu yang <snyangzhenyu99@gmail.com>
2014-10-03 23:43:44 -0700

    change the compiler version from 1.5 to 1.7

--- ch-simple-web/simple-webapp/src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
+++ ch-simple-web/simple-webapp/src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
@@ -7,9 +7,9 @@ import javax.servlet.http.*;
 public class SimpleServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response)
-        throws ServletException, IOException {
-	PrintWriter out = response.getWriter();
-	out.println( "SimpleServlet Executed" );
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("SimpleServlet Executed");
         out.flush();
         out.close();
     }
